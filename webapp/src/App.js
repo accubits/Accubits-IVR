70158e902ea6b1820e30d578d150aa0b4ef08449
john <john>
2020-02-12 12:37:06 +0530

    webapp

---
+++ webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './App.scss';
+import Dashboard from './pages/dashboard/dashboard';
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
