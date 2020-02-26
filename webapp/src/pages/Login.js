113b055e95f1b33b174dd0fd1a3dc52168f30bca
john <john>
2020-02-26 21:00:20 +0530

    login

--- webapp/src/pages/Login.js
+++ webapp/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button } from 'antd';
 
 import { userLogin, userRegister } from './../actions/user'
 import { Link } from 'react-router-dom';
@@ -21,7 +21,8 @@ class LoginForm extends Component {
         const { getFieldDecorator } = this.props.form;
         return (
             <div className="login-form">
-                <Form onSubmit={this.handleSubmit} className="login-form">
+                <img src="https://accubits.com/wp-content/uploads/2017/06/logo.png" alt="logo"></img>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.Item>
                         {getFieldDecorator('username', {
                             rules: [{ required: true, message: 'Please input your username!' }],
@@ -44,14 +45,16 @@ class LoginForm extends Component {
                         )}
                     </Form.Item>
                     <Form.Item>
-
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
+                        <div className="actions">
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
               </Button>
-                        Or  <Link to="/register">Register Now</Link>
+
+                            <Link to="/register">Register Now</Link>
+                        </div>
                     </Form.Item>
                 </Form>
-            </div>
+            </div >
         );
     }
 }
